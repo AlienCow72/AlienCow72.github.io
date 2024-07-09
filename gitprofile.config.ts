6824b8c0bd8d55effe47837d0ef69fb76f4298ad
AlienCow72 <56804191+AlienCow72@users.noreply.github.com>
2024-07-09 12:15:22 -0500

    Update gitprofile.config.ts
    test

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,12 +33,12 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Linktree Test',
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: 'https://linktree.kyleanderson.dev',
         },
         {
           title: 'Project Name',
